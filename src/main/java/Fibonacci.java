9fd635c2b79e47060b8798512e5737aa40908a13
Raul Mordillo <raul.mordillo.sge@mango.com>
2019-05-26 21:57:15 +0200

    Refactor to loop

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,17 +1,36 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Fibonacci {
     public String generate(int nthNumber) {
+
+        int[] sequence;
 
         if(nthNumber == 0){
             return "0";
         }else if(nthNumber == 1) {
 
-        return "0 1";
-        }else if(nthNumber == 2){
+            return "0 1";
 
-            return "0 1 1";
-        }else{
+        }else {
 
-            return "0 1 1 2";
+            sequence  = new int[nthNumber+1];
+            sequence[0] = 0;
+            sequence[1] = 1;
         }
+
+
+        int counter = 2;
+        while(counter <= nthNumber){
+
+            sequence[counter] = sequence[counter-1] + sequence[counter-2];
+            counter++;
+        }
+
+        return Arrays.stream(sequence)
+                .boxed()
+                .map(Object::toString)
+                .collect(Collectors.joining(" "));
+
     }
 }
